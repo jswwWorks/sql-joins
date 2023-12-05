7580fd60b810a6c6db5ba8250dedb55979a0b6a9
jswwWorks <jsww.works@gmail.com>
2023-12-05 15:11:36 -0500

    Pre code review queries

--- queries.sql
+++ queries.sql
@@ -1,1 +1,21 @@
--- write your queries here
+SELECT *
+    FROM owners
+        FULL JOIN vehicles
+            ON vehicles.owner_id = owners.id;
+
+
+SELECT owners.first_name, COUNT(*)
+    FROM vehicles
+        LEFT OUTER JOIN owners
+            ON vehicles.owner_id = owners.id
+    GROUP BY owners.id
+    ORDER BY owners.first_name;
+
+
+SELECT owners.first_name, AVG(vehicles.price)::integer, COUNT(*)
+    FROM vehicles
+            LEFT OUTER JOIN owners
+                ON vehicles.owner_id = owners.id
+    GROUP BY owners.id
+    HAVING COUNT(*) > 1 AND AVG(vehicles.price)::integer > 10000
+    ORDER BY owners.first_name DESC;
